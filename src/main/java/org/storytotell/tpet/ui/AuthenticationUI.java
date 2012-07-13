eceb6fe9ef0aea7094f6408517e63b328b93a4ca
Daniel Lyons <fusion@storytotell.org>
2012-07-13 01:03:18 -0600

    Initial integration with Apache Shiro.

---
+++ src/main/java/org/storytotell/tpet/ui/AuthenticationUI.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2012, Daniel Lyons
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+package org.storytotell.tpet.ui;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Daniel Lyons <fusion@storytotell.org>
+ */
+@RequestScoped
+@Named("authenticationUI")
+public class AuthenticationUI {
+  private static final Logger log = LoggerFactory.getLogger(AuthenticationUI.class);
+  private String username, password;
+
+  public @Produces Subject getCurrentUser() { 
+    return SecurityUtils.getSubject();
+  }
+  
+  public void authenticate() {
+    Subject currentUser = getCurrentUser();
+    
+    try {
+      currentUser.login(getAuthenticationToken());
+    } catch (Exception e) {
+      log.info("Login exception", e);
+    }
+  }
+  
+  public void logout() {
+    getCurrentUser().logout();
+  }
+  
+  public String getUsername() { return username; }
+  public String getPassword() { return password; }
+
+  public void setUsername(String username) { this.username = username; }
+  public void setPassword(String password) { this.password = password; }
+
+  private AuthenticationToken getAuthenticationToken() {
+    log.info("AuthenticationToken({}, {})", username, password);
+    return new UsernamePasswordToken(username, password);
+  }
+}
